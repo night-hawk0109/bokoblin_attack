dcc0c5eb2e8c55ef2decd7a5bb62209ce12d5e63
night-hawk0109 <houston.turner.payne@gmail.com>
2025-12-29 02:12:46 +0000

    added monsters

--- game.js
+++ game.js
@@ -6,7 +6,7 @@ let score = 0;
 // Player
 const player = {
 x: 180,
-y: 460,
+y: 440,
 width: 75,
 height: 65,
 speed: 5
@@ -16,6 +16,8 @@ speed: 5
 const shipImg = new Image();
 shipImg.src = "spaceship.gif";
 let bullets = [];
+let monsters = [];
+let spawnTimer = 0;
 let keys = {};
 
 // Controls
@@ -32,7 +34,7 @@ function shoot() {
 bullets.push({
 x: player.x + player.width / 2 - 2,
 y: player.y,
-speed: 7
+speed: 5
 });
 }
 
@@ -46,6 +48,38 @@ player.x += player.speed;
 // Move bullets
 bullets.forEach((b) => (b.y -= b.speed));
 bullets = bullets.filter((b) => b.y > 0);
+
+// Spawn monsters
+spawnTimer++;
+if (spawnTimer > 120) { // Spawn every 2 seconds at 60fps
+monsters.push({
+x: Math.random() * (canvas.width - 50),
+y: 0,
+width: 50,
+height: 50,
+speed: 2
+});
+spawnTimer = 0;
+}
+
+// Move monsters
+monsters.forEach((m) => (m.y += m.speed));
+monsters = monsters.filter((m) => m.y < canvas.height);
+
+// Collision detection
+bullets.forEach((b, bi) => {
+monsters.forEach((m, mi) => {
+if (b.x < m.x + m.width &&
+b.x + 4 > m.x &&
+b.y < m.y + m.height &&
+b.y + 10 > m.y) {
+// Hit!
+bullets.splice(bi, 1);
+monsters.splice(mi, 1);
+score++;
+}
+});
+});
 }
 
 function draw() {
@@ -65,6 +99,17 @@ ctx.fillStyle = "red";
 bullets.forEach((b) =>
 ctx.fillRect(b.x, b.y, 4, 10)
 );
+
+// Monsters
+ctx.fillStyle = "purple";
+monsters.forEach((m) =>
+ctx.fillRect(m.x, m.y, m.width, m.height)
+);
+
+// Score
+ctx.fillStyle = "white";
+ctx.font = "20px Arial";
+ctx.fillText("Score: " + score, 10, 30);
 }
 
 function loop() {
